904a24261bde83ab27727b3809607f69661f4dee
molzania <molzania_1507@yahoo.co.id>
2022-04-06 11:43:00 +0700

    jawab soal 4

--- models/soal4.js
+++ models/soal4.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
  // ini schema one-to-one schema
- 
+
 const soal4 = new mongoose.Schema({
   cinemaName: {
     type: String,
@@ -11,13 +11,13 @@ const soal4 = new mongoose.Schema({
       default:{
     name: {
         type: String,
-    },
+    }
   },
   location: {
       type: String,
       require: true,
   }
-});
+  }});
 
 mongoose.model("soal3", soal4);
 module.exports = soal4;
